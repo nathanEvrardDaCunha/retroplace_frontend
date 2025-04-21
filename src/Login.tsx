e72ad2b17e5394ef7ffecbef92f81a241d84474b
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-04-21 13:07:16 +0200

    feat(ui): implement basic html login structure

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,25 @@
 export function Login() {
-    // TODO: When DB and logic will be added, don't forget to implement an authentication system.
+    // TODO: When Backend implemented,  don't forget to implement an authentication system.
 
-    return <h1>Login For Now</h1>;
+    // TODO: When Backend implemented, don't forget to verify user data on the server instead of client.
+
+    // TODO: Should indicate to user which fields are required before he submit for the first time.
+
+    return (
+        <form>
+            <fieldset>
+                <legend>Log In to your Account</legend>
+
+                <label>Email</label>
+                <input type="email" placeholder="john.doe@gmail.com" required />
+
+                <label>Password</label>
+                <input type="password" placeholder="**************" required />
+
+                <button>Log In</button>
+                <button>Sign Up</button>
+                <button>Reset Password</button>
+            </fieldset>
+        </form>
+    );
 }
