3e4581278c53a89d6c011f218b608e1d325ed2ae
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-04-25 13:28:34 +0200

    feat(ui): make products gallery responsive

--- src/features/BrowseProduct.tsx
+++ src/features/BrowseProduct.tsx
@@ -1,129 +1,133 @@
+import './BrowseProduct.scss';
+
 export function BrowseProduct() {
     // IDEA: Use "main" instead
     return (
-        <main>
+        <main className="main">
             {/* // DESIGN: One section to filter the products.
         // => Console System, Type of Product (Video Game, Figurines, Consoles, Limited Edition Stuff...)... */}
 
-            <section>
+            <section className="filter">
                 <h2>Filter available products</h2>
             </section>
 
             {/* // DESIGN: One section to sort the products.
         // => By Age, By Used Quality, By Price... */}
-            <section>
+            <section className="sort">
                 <h2>Sort available products</h2>
             </section>
 
             {/* // DESIGN: One section to browse the remaining product. */}
 
-            <section>
-                <h2>Browse available products</h2>
+            <section className="browse">
+                <h2 className="browse__title">Browse available products</h2>
 
-                <article>
-                    <figure>
-                        <img src="https://picsum.photos/200" alt="lorem picsum" />
-                        <figcaption>Lorem picsum</figcaption>
-                    </figure>
+                <section className="browse__products">
+                    <article>
+                        <figure>
+                            <img src="https://picsum.photos/200" alt="lorem picsum" />
+                            <figcaption>Lorem picsum</figcaption>
+                        </figure>
 
-                    {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
-                    <section>
-                        <h4>Nintendo 3DS</h4>
-                        <p>12/01/2013</p>
-                        <p>Slightly Used</p>
-                        <p>13.99€</p>
-                    </section>
+                        {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
+                        <section>
+                            <h4>Nintendo 3DS</h4>
+                            <p>12/01/2013</p>
+                            <p>Slightly Used</p>
+                            <p>13.99€</p>
+                        </section>
 
-                    {/* TODO: Return to the product page */}
-                    {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
-                    <section>
-                        <button>See More</button>
-                    </section>
-                </article>
+                        {/* TODO: Return to the product page */}
+                        {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
+                        <section>
+                            <button>See More</button>
+                        </section>
+                    </article>
 
-                <article>
-                    <figure>
-                        <img src="https://picsum.photos/200" alt="lorem picsum" />
-                        <figcaption>Lorem picsum</figcaption>
-                    </figure>
+                    <article>
+                        <figure>
+                            <img src="https://picsum.photos/200" alt="lorem picsum" />
+                            <figcaption>Lorem picsum</figcaption>
+                        </figure>
 
-                    {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
-                    <section>
-                        <h4>Nintendo 3DS</h4>
-                        <p>12/01/2013</p>
-                        <p>Slightly Used</p>
-                        <p>13.99€</p>
-                    </section>
+                        {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
+                        <section>
+                            <h4>Nintendo 3DS</h4>
+                            <p>12/01/2013</p>
+                            <p>Slightly Used</p>
+                            <p>13.99€</p>
+                        </section>
 
-                    {/* TODO: Return to the product page */}
-                    {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
-                    <section>
-                        <button>See More</button>
-                    </section>
-                </article>
+                        {/* TODO: Return to the product page */}
+                        {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
+                        <section>
+                            <button>See More</button>
+                        </section>
+                    </article>
 
-                <article>
-                    <figure>
-                        <img src="https://picsum.photos/200" alt="lorem picsum" />
-                        <figcaption>Lorem picsum</figcaption>
-                    </figure>
+                    <article>
+                        <figure>
+                            <img src="https://picsum.photos/200" alt="lorem picsum" />
+                            <figcaption>Lorem picsum</figcaption>
+                        </figure>
 
-                    {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
-                    <section>
-                        <h4>Nintendo 3DS</h4>
-                        <p>12/01/2013</p>
-                        <p>Slightly Used</p>
-                        <p>13.99€</p>
-                    </section>
+                        {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
+                        <section>
+                            <h4>Nintendo 3DS</h4>
+                            <p>12/01/2013</p>
+                            <p>Slightly Used</p>
+                            <p>13.99€</p>
+                        </section>
 
-                    {/* TODO: Return to the product page */}
-                    {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
-                    <section>
-                        <button>See More</button>
-                    </section>
-                </article>
+                        {/* TODO: Return to the product page */}
+                        {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
+                        <section>
+                            <button>See More</button>
+                        </section>
+                    </article>
 
-                <article>
-                    <figure>
-                        <img src="https://picsum.photos/200" alt="lorem picsum" />
-                        <figcaption>Lorem picsum</figcaption>
-                    </figure>
+                    <article>
+                        <figure>
+                            <img src="https://picsum.photos/200" alt="lorem picsum" />
+                            <figcaption>Lorem picsum</figcaption>
+                        </figure>
 
-                    {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
-                    <section>
-                        <h4>Nintendo 3DS</h4>
-                        <p>12/01/2013</p>
-                        <p>Slightly Used</p>
-                        <p>13.99€</p>
-                    </section>
+                        {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
+                        <section>
+                            <h4>Nintendo 3DS</h4>
+                            <p>12/01/2013</p>
+                            <p>Slightly Used</p>
+                            <p>13.99€</p>
+                        </section>
 
-                    {/* TODO: Return to the product page */}
-                    {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
-                    <section>
-                        <button>See More</button>
-                    </section>
-                </article>
+                        {/* TODO: Return to the product page */}
+                        {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
+                        <section>
+                            <button>See More</button>
+                        </section>
+                    </article>
 
-                <article>
-                    <figure>
-                        <img src="https://picsum.photos/200" alt="lorem picsum" />
-                        <figcaption>Lorem picsum</figcaption>
-                    </figure>
+                    <article>
+                        <figure>
+                            <img src="https://picsum.photos/200" alt="lorem picsum" />
+                            <figcaption>Lorem picsum</figcaption>
+                        </figure>
 
-                    {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
-                    <section>
-                        <h4>Nintendo 3DS</h4>
-                        <p>12/01/2013</p>
-                        <p>Slightly Used</p>
-                        <p>13.99€</p>
-                    </section>
+                        {/* IDEA: See to add a 0/5 rating information about the seller now or later. */}
+                        <section>
+                            <h4>Nintendo 3DS</h4>
+                            <p>12/01/2013</p>
+                            <p>Slightly Used</p>
+                            <p>13.99€</p>
+                        </section>
 
-                    {/* TODO: Return to the product page */}
-                    {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
-                    <section>
-                        <button>See More</button>
-                    </section>
-                </article>
+                        {/* TODO: Return to the product page */}
+                        {/* IDEA: Allow users to report, on the detailed page, the product if it infringe our policy */}
+                        <section>
+                            <button>See More</button>
+                        </section>
+                    </article>
+                </section>
             </section>
         </main>
     );
--- src/pages/shared/NotFoundPage.tsx
+++ src/pages/shared/NotFoundPage.tsx
@@ -1,6 +1,5 @@
 import { FooterLayout } from '../../layouts/FooterLayout';
 import { HeaderLayout } from '../../layouts/HeaderLayout';
-import { MainLayout } from '../../layouts/MainLayout';
 
 export function NotFoundPage() {
     // TODO: Add a link that return to the previous page or home page.
