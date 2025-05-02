84ca56baae86ae8bd164f98d947ea0d680ebcf95
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-02 18:05:29 +0200

    feat(ui): implement new modular link UI primitive

--- src/components/ui/CustomLink.tsx
+++ src/components/ui/CustomLink.tsx
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom';
+import './ui-style.scss';
 
 interface CustomLinkProps {
     to: string;
-    children: React.ReactNode | string;
+    children: string;
+    style: 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'small';
 }
 
+// IDEA: Make to optional with ternary
+// IDEA: Make style optional with ternary
+
 export function CustomLink(props: CustomLinkProps) {
-    return <Link to={props.to}>{props.children}</Link>;
+    const className = `link--${props.style}`;
+
+    return (
+        <Link className={className} to={props.to}>
+            {props.children}
+        </Link>
+    );
 }
